e0de4240cb00e9088bef6a1b80d084ba5eadb106
Slity <84740013+vni911@users.noreply.github.com>
2024-10-09 03:50:26 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a =int(input())
+if (a>=90) :
+    print("A")
+elif (80 <= a <90) :
+    print("B")
+elif (70 <= a <80) :
+    print("C")
+elif (60 <= a <70) :
+    print("D")
+else :
+    print("F")
